d0553b69d5b06347512d7d792c58f36e3dd9f01f
Josie <josiekwheeler@gmail.com>
2019-05-29 10:34:29 -0400

    did nothing

--- src/app/search-criteria/search-criteria.component.ts
+++ src/app/search-criteria/search-criteria.component.ts
@@ -9,7 +9,7 @@ import { RecipeApiService } from "../recipe-api.service";
 export class SearchCriteriaComponent implements OnInit {
   recipeData: any[] = [];
 
-  constructor(private recipeApiService: RecipeApiService) {}
+  constructor(private recipeApiService: RecipeApiService) { }
 
   ngOnInit() {
     this.recipeData = this.recipeApiService.getRecipes();
@@ -26,5 +26,11 @@ export class SearchCriteriaComponent implements OnInit {
       .then(response => {
         this.recipeData = response;
       });
+  }
+
+
+
+  deleteFromFavorites(index: number) {
+    this.recipeData.splice(index, 1);
   }
 }
